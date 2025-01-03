f7438943e0254ffec311159d5c6dc3fe2028d126
T-zi89 <piaiatiziano@gmail.com+odin@users.noreply.github.com>
2025-01-03 11:00:05 +0100

    complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
-};
+const leapYears = function(year) {
+    if (year % 4 === 0) {
+        if (year % 4 === 0 && year % 100 === 0) {
+            if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+                return true;
+            }
+            return false;
+        }
+    return true;
+    } else {
+    return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = leapYears;
